38e4a59d5f09cb2eb4383f400f083f35cf961f30
Yanick <yanick@bu.edu>
2022-06-23 16:48:43 -0500

    updated css

--- src/Submission.jsx
+++ src/Submission.jsx
@@ -3,6 +3,15 @@ import Modal from 'react-modal';
 import './Submission.css';
 import axios from 'axios';
 
+const style = {
+  overlay: {
+    backgroundColor: '#B0D0D3',
+  },
+  content: {
+    background: '#151E3F',
+  },
+};
+
 class Submission extends React.Component {
   constructor(props) {
     super(props);
@@ -56,8 +65,14 @@ class Submission extends React.Component {
   render() {
     const { modalOpen } = this.state;
     return (
-      <Modal isOpen={modalOpen} appElement={document.getElementById('root')}>
+      <Modal style={style} isOpen={modalOpen} appElement={document.getElementById('root')}>
         <div className="styleContainer">
+          <div onClick={this.close} className="closeDiv">
+            <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="currentColor" className="bi-x-square" viewBox="0 0 16 16">
+              <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"/>
+              <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
+            </svg>
+          </div>
           <h3>Idea Submission Form</h3>
           <div>Subject:</div>
           <input
